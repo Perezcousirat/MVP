f9416e24fbb6d0a45de87002789f86de7ad5a670
Alejandro Perez <perez.cousirat.alejandro@gmail.com>
2023-05-29 02:12:46 -0300

    update

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    def __str__(self):
+        return self.titulo
+    
+    
+class Autor(models.Model):
+    nombre = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre
